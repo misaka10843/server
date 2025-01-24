8f740ccd9bf695dc5c261daf79f4ff3a6e787f00
thaumstrial <2738722282@qq.com>
2025-01-24 23:02:00 +0530

    Implement find_by_id for song controller

--- src/controller/song.rs
+++ src/controller/song.rs
@@ -1,5 +1,7 @@
 use axum::extract::State;
 use axum::Json;
+use entity::song;
+use sea_orm::EntityName;
 use serde::Deserialize;
 use utoipa::ToSchema;
 use utoipa_axum::router::OpenApiRouter;
@@ -13,7 +15,9 @@ use crate::state::AppState;
 type Error = GeneralRepositoryError;
 
 pub fn router() -> OpenApiRouter<AppState> {
-    OpenApiRouter::new().routes(routes!(create_song))
+    OpenApiRouter::new()
+        .routes(routes!(create_song))
+        .routes(routes!(find_by_id))
 }
 
 #[derive(ToSchema, Deserialize)]
@@ -22,13 +26,18 @@ struct CreateSongInput {
     pub data: NewSong,
 }
 
+#[derive(ToSchema, Deserialize)]
+struct FindSongByIdInput {
+    pub id: i32,
+}
+
 #[utoipa::path(
     post,
     path = "/song",
     request_body = CreateSongInput,
     responses(
-		(status = 200, description = "Song create successfully"),
-		(status = 500, description = "Song create failed"),
+		(status = 200, description = "Song created successfully"),
+		(status = 500, description = "Failed to create song"),
     ),
 )]
 async fn create_song(
@@ -37,4 +46,24 @@ async fn create_song(
 ) -> Result<(), Error> {
     service.create(input.data).await?;
     Ok(())
+}
+
+#[utoipa::path(
+    post,
+    path = "/song",
+    request_body = FindSongByIdInput,
+    responses(
+		(status = 200, description = "Song found by id"),
+		(status = 500, description = "Failed to find song by id"),
+    ),
+)]
+async fn find_by_id(
+    State(service): State<SongService>,
+    Json(input): Json<FindSongByIdInput>,
+) -> Result<Json<song::Model>, Error> {
+    Ok(Json(service.find_by_id(input.id).await?.ok_or(
+        Error::EntityNotFound {
+            entity_name: song::Entity.table_name(),
+        },
+    )?))
 }
