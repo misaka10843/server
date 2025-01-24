18784a823a8d3fd558375a79ed90ed462fe39cdd
thaumstrial <2738722282@qq.com>
2025-01-24 23:20:35 +0530

    Implement random for song controller

--- src/controller/release.rs
+++ src/controller/release.rs
@@ -1,6 +1,7 @@
 use axum::extract::State;
 use axum::response::IntoResponse;
 use axum::Json;
+use sea_orm::EntityName;
 use serde::Deserialize;
 use utoipa::ToSchema;
 use utoipa_axum::router::OpenApiRouter;
@@ -8,6 +9,7 @@ use utoipa_axum::routes;
 
 use crate::dto::correction::Metadata;
 use crate::dto::release::GeneralRelease;
+use crate::error::GeneralRepositoryError;
 use crate::service::release::ReleaseService;
 use crate::state::AppState;
 use crate::{repo, service};
@@ -31,7 +33,9 @@ impl IntoResponse for repo::release::Error {
 }
 
 pub fn router() -> OpenApiRouter<AppState> {
-    OpenApiRouter::new().routes(routes!(create_release))
+    OpenApiRouter::new()
+        .routes(routes!(create_release))
+        .routes(routes!(find_by_id))
 }
 
 #[derive(ToSchema, Deserialize)]
@@ -39,6 +43,16 @@ struct CreateReleaseInput {
     #[serde(flatten)]
     pub data: GeneralRelease,
     pub correction_data: Metadata,
+}
+
+#[derive(ToSchema, Deserialize)]
+struct FindReleaseByIdInput {
+    pub id: i32,
+}
+
+#[derive(ToSchema, Deserialize)]
+struct RandomReleaseInput {
+    pub count: u64,
 }
 
 #[utoipa::path(
@@ -56,4 +70,42 @@ async fn create_release(
 ) -> Result<(), Error> {
     service.create(input.data, input.correction_data).await?;
     Ok(())
+}
+
+#[utoipa::path(
+    post,
+    path = "/release",
+    request_body = FindReleaseByIdInput,
+    responses(
+		(status = 200, description = "Release found by id"),
+		(status = 500, description = "Failed to find release by id"),
+    ),
+)]
+async fn find_by_id(
+    State(service): State<ReleaseService>,
+    Json(input): Json<FindReleaseByIdInput>,
+) -> Result<Json<entity::release::Model>, Error> {
+    Ok(Json(service.find_by_id(input.id).await?.ok_or(
+        Error::Repo(repo::release::Error::General(
+            GeneralRepositoryError::EntityNotFound {
+                entity_name: entity::release::Entity.table_name(),
+            },
+        )),
+    )?))
+}
+
+#[utoipa::path(
+    post,
+    path = "/release",
+    request_body = RandomReleaseInput,
+    responses(
+		(status = 200, description = "Release found by id"),
+		(status = 500, description = "Failed to find release by id"),
+    ),
+)]
+async fn random(
+    State(service): State<ReleaseService>,
+    Json(input): Json<RandomReleaseInput>,
+) -> Result<Json<Vec<entity::release::Model>>, Error> {
+    Ok(Json(service.random(input.count).await?))
 }
