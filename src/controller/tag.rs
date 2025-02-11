84e5e48d374c7b7f2c886ad87975c9d375677441
Huli <134658521+Huliiiiii@users.noreply.github.com>
2025-02-11 10:02:31 +0800

    Impl `upsert_tag_correction` for tag

--- src/controller/tag.rs
+++ src/controller/tag.rs
@@ -1,6 +1,7 @@
 use axum::Json;
-use axum::extract::State;
-use macros::use_session;
+use axum::extract::{Path, State};
+use axum::middleware::from_fn;
+use macros::{use_service, use_session};
 use serde::Deserialize;
 use utoipa::ToSchema;
 use utoipa_axum::router::OpenApiRouter;
@@ -10,23 +11,28 @@ use crate::api_response;
 use crate::dto::correction::Metadata;
 use crate::dto::tag::NewTag;
 use crate::error::RepositoryError;
+use crate::middleware::is_signed_in;
 use crate::service::tag::Service;
 use crate::state::AppState;
 pub fn router() -> OpenApiRouter<AppState> {
-    OpenApiRouter::new().routes(routes!(create_tag))
+    OpenApiRouter::new()
+        .routes(routes!(create_tag))
+        .routes(routes!(upsert_tag_correction))
+        .route_layer(from_fn(is_signed_in))
 }
 
 #[derive(ToSchema, Deserialize)]
-struct CreateTagInput {
+struct TagCorrection {
     #[serde(flatten)]
+    #[schema(inline)]
     pub data: NewTag,
-    pub correction_data: Metadata,
+    pub correction_metadata: Metadata,
 }
 
 #[utoipa::path(
     post,
     path = "/tag",
-    request_body = CreateTagInput,
+    request_body = TagCorrection,
     responses(
 		(status = 200, body = api_response::Message),
 		(status = 401),
@@ -36,11 +42,40 @@ struct CreateTagInput {
 #[use_session]
 async fn create_tag(
     State(service): State<Service>,
-    Json(input): Json<CreateTagInput>,
+    Json(input): Json<TagCorrection>,
 ) -> Result<api_response::Message, RepositoryError> {
     let user_id = session.user.unwrap().id;
     service
-        .create(user_id, input.data, input.correction_data)
+        .create(user_id, input.data, input.correction_metadata)
         .await?;
+    Ok(api_response::Message::ok())
+}
+
+#[utoipa::path(
+    post,
+    path = "/tag/{id}",
+    request_body = TagCorrection,
+    responses(
+		(status = 200, body = api_response::Message),
+		(status = 401),
+        RepositoryError
+    ),
+)]
+#[use_session]
+#[use_service(tag)]
+async fn upsert_tag_correction(
+    Path(id): Path<i32>,
+    Json(input): Json<TagCorrection>,
+) -> Result<api_response::Message, RepositoryError> {
+    let user_id = session.user.unwrap().id;
+    tag_service
+        .upsert_correction()
+        .tag_id(id)
+        .user_id(user_id)
+        .data(input.data)
+        .correction_metadata(input.correction_metadata)
+        .call()
+        .await?;
+
     Ok(api_response::Message::ok())
 }
--- src/service/tag.rs
+++ src/service/tag.rs
@@ -1,5 +1,6 @@
+use entity::sea_orm_active_enums::EntityType;
 use entity::tag;
-use sea_orm::TransactionTrait;
+use sea_orm::{DatabaseConnection, TransactionTrait};
 
 use crate::dto::correction::Metadata;
 use crate::dto::tag::NewTag;
@@ -8,18 +9,95 @@ use crate::repo;
 
 super::def_service!();
 
+#[bon::bon]
 impl Service {
     pub async fn create(
         &self,
         user_id: i32,
-        tag_data: NewTag,
+        data: NewTag,
         correction_data: Metadata,
     ) -> Result<tag::Model, RepositoryError> {
         let transaction = self.db.begin().await?;
         let result =
-            repo::tag::create(user_id, tag_data, correction_data, &transaction)
+            repo::tag::create(user_id, data, correction_data, &transaction)
                 .await?;
         transaction.commit().await?;
         Ok(result)
     }
+
+    #[builder]
+    pub async fn upsert_correction(
+        &self,
+        user_id: i32,
+        tag_id: i32,
+        data: NewTag,
+        correction_metadata: Metadata,
+    ) -> Result<(), RepositoryError> {
+        let tx = self.db.begin().await?;
+
+        let res = super::correction::create_or_update_correction()
+            .entity_id(tag_id)
+            .entity_type(EntityType::Tag)
+            .user_id(user_id)
+            .closure_args((data, correction_metadata))
+            .on_create(|_, (data, metadata)| {
+                repo::tag::create_update_correction(
+                    tag_id, user_id, data, metadata, &tx,
+                )
+            })
+            .on_update(|correction, (data, metadata)| {
+                repo::tag::update_update_correction(
+                    user_id, correction, data, metadata, &tx,
+                )
+            })
+            .db(&self.db)
+            .call()
+            .await;
+
+        tx.commit().await?;
+
+        res
+    }
+}
+
+async fn create_correction(
+    tag_id: i32,
+    user_id: i32,
+    data: NewTag,
+    correction_metadata: Metadata,
+    db: &DatabaseConnection,
+) -> Result<(), RepositoryError> {
+    let tx = db.begin().await?;
+
+    repo::tag::create_update_correction(
+        tag_id,
+        user_id,
+        data,
+        correction_metadata,
+        &tx,
+    )
+    .await?;
+
+    tx.commit().await?;
+
+    Ok(())
+}
+
+async fn update_correction(
+    user_id: i32,
+    correction: entity::correction::Model,
+    data: NewTag,
+    metadata: Metadata,
+    db: &DatabaseConnection,
+) -> Result<(), RepositoryError> {
+    let tx = db.begin().await?;
+
+    repo::tag::update_update_correction(
+        user_id, correction, data, metadata, &tx,
+    )
+    .await?;
+
+    tx.commit().await?;
+
+    Ok(())
 }
