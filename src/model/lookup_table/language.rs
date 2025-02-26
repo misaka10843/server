d02312f2cf7a1f738af60b8b839cc2c3afc486f0
Huli <134658521+Huliiiiii@users.noreply.github.com>
2025-02-26 09:44:51 +0800

    😅

--- src/model/lookup_table/language.rs
+++ src/model/lookup_table/language.rs
@@ -11,10 +11,31 @@ struct Lang {
 }
 
 pub async fn upsert_langauge(db: &impl ConnectionTrait) -> Result<(), DbErr> {
-    let json = include_str!("iso-639-3.json");
-    let data = serde_json::from_str::<Vec<Lang>>(json)
-        .unwrap()
+    let langs = [
+        ("Cantonese", "yue"),
+        ("Chinese", "zho"),
+        ("English", "eng"),
+        ("Finnish", "fin"),
+        ("French", "fra"),
+        ("German", "deu"),
+        ("Italian", "ita"),
+        ("Japanese", "jpn"),
+        ("Korean", "kor"),
+        ("Latin", "lat"),
+        ("Mandarin", "cmn"),
+        ("Min Nan Chinese", "nan"),
+        ("Polish", "pol"),
+        ("Russian", "rus"),
+        ("Spanish", "spa"),
+        ("Swedish", "swe"),
+    ]; // Append only after deployment
+
+    let data = langs
         .into_iter()
+        .map(|x| Lang {
+            name: x.0.to_string(),
+            code: x.1.to_string(),
+        })
         .enumerate()
         .collect_vec();
 
